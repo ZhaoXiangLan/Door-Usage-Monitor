fd710501d879110d3f9e1458b2a5feb84df776af
ZhaoXiang Lan <tuv12540@temple.edu>
2026-04-20 11:44:10 -0400

    Update the esp32 code  which connect to the EnterpriseWifi

--- Esp32-Door-Usage-Monitor/src/wifi_connect.cpp
+++ Esp32-Door-Usage-Monitor/src/wifi_connect.cpp
@@ -3,34 +3,29 @@
 #include "wifi_connect.h"
 #include "secrets.h"
 
-void connectWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect(true, true);
-  delay(1000);
+extern "C" {
+  #include "esp_wpa2.h"
+  #include "esp_wifi.h"
+}
 
+void connectWiFi() {
   Serial.println("Trying multiple WiFi networks...");
 
   for (int i = 0; i < wifiCount; i++) {
-    Serial.println();
     Serial.print("Connecting to: ");
     Serial.println(ssidList[i]);
-
-    WiFi.disconnect(true, true);
-    delay(1000);
 
     WiFi.begin(ssidList[i], passwordList[i]);
 
     int retry = 0;
-    while (WiFi.status() != WL_CONNECTED && retry < 20) {
-      delay(500);
+    while (WiFi.status() != WL_CONNECTED && retry < 10) {
+      delay(1000);
       Serial.print(".");
       retry++;
     }
 
-    Serial.println();
-
     if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Connected!");
+      Serial.println("\nConnected!");
       Serial.print("SSID: ");
       Serial.println(ssidList[i]);
       Serial.print("IP: ");
@@ -38,9 +33,46 @@ void connectWiFi() {
       return;
     }
 
-    Serial.print("Failed. Status = ");
-    Serial.println(WiFi.status());
+    Serial.println("\nFailed, trying next...\n");
   }
 
   Serial.println("Could not connect to any WiFi.");
+}
+
+void connectEnterpriseWiFi() {
+  Serial.println("Trying to connect to Enterprise WiFi...");
+
+  WiFi.disconnect(true);
+  delay(500);
+  WiFi.mode(WIFI_STA);
+
+  if (enterpriseSSID == nullptr || enterpriseUsername == nullptr || enterprisePassword == nullptr) {
+    Serial.println("Enterprise WiFi config missing.");
+    return;
+  }
+
+  WiFi.begin(
+    enterpriseSSID,
+    WPA2_AUTH_PEAP,
+    enterpriseUsername,   // identity
+    enterpriseUsername,   // username
+    enterprisePassword    // password
+  );
+
+  int retry = 0;
+  while (WiFi.status() != WL_CONNECTED && retry < 15) {
+    delay(1000);
+    Serial.print(".");
+    retry++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConnected to Enterprise WiFi!");
+    Serial.print("SSID: ");
+    Serial.println(enterpriseSSID);
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nFailed to connect to Enterprise WiFi.");
+  }
 }
