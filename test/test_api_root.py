b342d13c0ddd5193ff91998d4b20c90dac076e4b
ZhaoXiang Lan <tuv12540@temple.edu>
2026-04-20 18:28:01 -0400

    update the test case

--- test/test_api_root.py
+++ test/test_api_root.py
@@ -5,3 +5,17 @@ def test_root(client):
 
     assert response.status_code == 200
     assert response.json() == {"message": "Server is working"}
+# Test entire flow
+def test_full_api_flow(client):
+    test_client, _ = client
+
+    test_client.post(
+        "/api/data",
+        headers={"x-api-key": "test-api-key"},
+        json={"state": "open", "device": "Door_1"}
+    )
+
+    response = test_client.get("/api/data")
+    data = response.json()
+
+    assert len(data["raw_data"]) == 1
